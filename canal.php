bec2d756b134b9acf951df6a053bbd0497cb57da
MrGmaw <63761632+MrGmaw@users.noreply.github.com>
2022-09-02 14:30:50 -0500

    Delete canal.php

--- canal.php
+++
@@ -1,8 +0,0 @@
-<?php
-$st=$_GET["st"];
-$ch=$_GET["ch"];
-$res=$_GET["r1"].'x'.$_GET["r2"];
-$loc='Location: https://v-'.$st.'.mybtv.net/live/'.$ch.'/'.$res.'.m3u8?env=prd&uid=-1&x=1';
-        header("Content-type: text/plain");
-        header($loc);
-?>
